802e78c8d12379f78ba2b8d03da6de2b4dd7a5b7
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-16 15:23:12 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,30 @@
+//first include library
+#include<iostream>
+using namespace std;
+//maths codes begin
+int main()
+//process
+{
+int a,b,addition,substraction,multiplication,division,modulus;
+	a=50;
+	b=25;
+//adding integers;
+addition=a+b; 
+//substracting integers;
+substraction=a-b;
+//multiply integers; 
+multiplication=a*b; 
+//devide integers;
+division=a/b; 
+//showing remainder;
+modulus=a%b; 
+
+
+cout<<addition<<endl;
+cout<<substraction<<endl;
+cout<<multiplication<<endl;
+cout<<division<<endl;
+cout<<modulus<<endl;
+return 0;
+}
+
