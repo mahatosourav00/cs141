3594223084c16b08e7d1ca8ac39ac63437767d85
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-16 14:58:04 +0000

    Add files via upload

---
+++ SRV.cpp
@@ -0,0 +1,22 @@
+//first include the library
+#include<iostream>
+using namespace std;
+//write the main function
+int main()
+//process
+{
+//declaring variable
+char a=1;
+int b='2';
+float c=2.007;
+double d=2.01;
+bool e=0;
+//will get size of variables
+cout <<"size of char;"<<sizeof(char)<<"byte"<<"contents of a-"<<a<<endl;
+cout <<"size of int;"<<sizeof(int)<<"byte"<<"contents of b="<<b<<endl;
+cout <<"size of float;"<<sizeof(float)<<"byte"<<"contents of c="<<c<<endl;
+cout <<"size of double;"<<sizeof(double)<<"byte"<<"contents of d="<<d<<endl;
+cout <<"size of bool;"<<sizeof(bool)<<"byte"<<"contents of e="<<e<<endl;
+return 0;
+}
+
