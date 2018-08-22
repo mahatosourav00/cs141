eff8b4cbf2c9032aed8364bc6af6699fbf770203
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-22 06:19:35 +0000

    Add files via upload

---
+++ lab4_q5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+//first enter values
+	double angle1,angle2,angleX;
+//Question no.5
+	cout<<"Enter 1st angle"<<endl;
+	cin>>angle1;
+	cout<<"Enter 2nd angle"<<endl;
+	cin>>angle2;
+	angleX=180-(angle1+angle2);
+//Answer
+	cout<<"The 3rd angle is="<<angleX<<endl;
+}
+//done
