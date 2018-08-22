9309f70d5f5df65c41376a705398fade970dc120
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-22 06:28:04 +0000

    Add files via upload

---
+++ lab4_q6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+//first enter values
+	double base,height,area;
+//Question no 6
+	cout<<"enter the base of the triangle"<<endl;
+	cin>>base;
+	cout<<"enter the height of the triangle"<<endl;
+	cin>>height;
+	area=0.5*base*height;
+//Answer
+	cout<<"the area of the triangle="<<area<<endl;
+}
+//done
+
+
+
