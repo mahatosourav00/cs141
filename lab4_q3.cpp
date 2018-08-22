e369c13657e070bbc33a9f13e1e59b48e1360c85
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-22 05:37:56 +0000

    Add files via upload

---
+++ lab4_q3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+//Enter the value
+	double fahrenheit,celsius,result;
+//Question no. 3
+	cout<<"enter value on fahrenheit"<<endl;
+	cin>>fahrenheit;
+	result=fahrenheit-32;
+	celsius=result*5/9;
+
+//Answer
+	cout<<"The answer in fahrenheit is"<<celsius<<endl;
+}
+//done
