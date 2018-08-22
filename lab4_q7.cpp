46821caa45b461ccb47c1337cf341b3cdf8e03e0
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-22 06:38:16 +0000

    Add files via upload

---
+++ lab4_q7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+//first enter values
+	double side,area;
+//Question no. 7
+	cout<<"enter a side of the equilateral triangle"<<endl;
+	cin>>side;
+	area=(1.732/4)*(side*side);
+//Answer
+	cout<<"the area of equilateral triangle is="<<area<<endl;
+}
+//done
