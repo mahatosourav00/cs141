64d04406a792683eea0f41067ea929d5ba06b7e6
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-22 05:00:34 +0000

    Add files via upload

---
+++ lab4_q1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+//first enter values
+	double length;
+	double resultM,resultKM;
+//question no. 1
+	cout<<"enter value on centimeter"<<endl;
+	cin>>length;
+	resultM=length/100;
+	resultKM=length/100000;
+// the answer
+	cout<<"the answer is="<<resultM<<endl;
+	cout<<"the answer is="<<resultKM<<endl;
+}
+//done
+
+
