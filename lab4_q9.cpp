98f0c5465501b229e7f487ea5b18dfdde3df0e81
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-22 07:16:26 +0000

    Add files via upload

---
+++ lab4_q9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+//enter values
+	double principle,time,rate,interest;
+//Question no. 9
+	cout<<"we have to calculate simple interest so ";
+	cout<<"enter the principle"<<endl;
+	cin>>principle;
+	cout<<"enter the rate"<<endl;
+	cin>>rate;
+	cout<<"enter the time"<<endl;
+	cin>>time;
+	interest=(rate/100)*principle;
+//Answer
+	cout<<"the simple interest is="<<interest<<endl;
+}
+//done
+	
