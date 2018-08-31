3dbbfca2ce5f8ef30cb5e452261b42d73f6f1e4a
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-31 11:35:49 +0530

    Add files via upload

---
+++ lab5_q5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+// asiggning the value
+	int a;
+//question no. 5
+	cout<<"enter the number"<<endl;
+	cin>>a;
+//answer
+	if (a==0)
+	cout<<"number is zero"<<endl;
+	else if (a%2==0)
+	cout<<"number is even"<<endl;
+	else
+	cout<<"number is odd"<<endl;
+//done
+return 0;
+}
