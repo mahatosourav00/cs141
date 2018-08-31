920f800835ad831b4356d38653bb46f536330bf3
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-31 10:55:14 +0530

    Add files via upload

---
+++ lab5_q3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+// asiggning the value
+	int a;
+//qustion no. 3
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+//answer
+	if (a>0)
+	cout<<"a is positive"<<endl;
+	else if (a==0)
+	cout<<"a is equal to zero"<<endl;
+	else
+	cout<<"a is negetive"<<endl;
+//done
+return 0;
+}
