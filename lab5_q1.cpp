8422e7040a357ee90ec18baf35d81ec73ad0ac41
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-31 10:24:48 +0530

    Add files via upload

---
+++ lab5_q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+// asiggning the value
+	int a,b;
+//question no. 1
+	cout<<"what is the value of a?"<<endl;
+	cin>>a;
+	cout<<"what is the value of b?"<<endl;
+	cin>>b;
+// The answer
+	if (a>b)
+	cout<<"a is greater than b"<<endl;
+	else if (a==b)
+	cout<<"a and b are same"<<endl;
+	else
+	cout<<"b is greater than a"<<endl;
+
+//Done
+return 0;
+}
