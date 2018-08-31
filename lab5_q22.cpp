02ee6a2116fc81b48302c0eeeb08839e67b9fd9e
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-31 12:17:52 +0530

    Add files via upload

---
+++ lab5_q22.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+// asiggning the value
+	int i, n;
+	i=0;
+	cout<<" enter a number n to see all the natural number upto n"<<endl;
+	cin>>n;
+	while(i<n){
+	i++;
+	cout<<i<<endl;
+}
+return 0;
+}
