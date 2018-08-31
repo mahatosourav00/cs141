d1ebe034441a2fa9ef3381b000ecd8ece9421b01
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-31 20:49:02 +0530

    Add files via upload

---
+++ lab5_q24.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+//assign the value
+	char b, y; 
+	b='a';
+//taking input
+	cout<<"want to see alphabets? Y/N"<<endl;
+	cin>>y;
+	if (y=='Y'){
+	cout<<b<<endl;
+	while (b<='z'){
+	cout<<b<<endl;
+	b++;
+	}}
+	else
+	cout<<"go to hell"<<endl;
+return 0;
+}
+
