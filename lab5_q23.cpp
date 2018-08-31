aacf720a7ef93ead638257a51898cd78baaf8b98
mahatosourav00 <42140479+mahatosourav00@users.noreply.github.com>
2018-08-31 19:31:59 +0530

    Add files via upload

---
+++ lab5_q23.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+//assign the value
+	int i, n;
+	i=1;
+//taking input
+	cout<<"enter number n so that u can see all natural no. upto 1"<<endl;
+	cin>>n;
+//answer
+	while(n>i){
+	n=n-1;
+	cout<<n<<endl;
+	}
+//done
+return 0;
+}
